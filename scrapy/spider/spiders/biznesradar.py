e691dbccf089b9fc7c3a9f7179b577b439dffd93
Mateusz Banaś <m.banas@createit.pl>
2018-10-28 00:23:45 +0200

    Extracting diff data from biznesradar financial statements

--- scrapy/spider/spiders/biznesradar.py
+++ scrapy/spider/spiders/biznesradar.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import zipfile, io, os, csv
+import zipfile, io, os, csv, re
 import pandas as pd
 from time import sleep
 
@@ -67,10 +67,58 @@ class BiznesradarSpider(scrapy.Spider):
         quarters = response.css(base_css + 'th.thq::text').extract()
         labels = response.css(base_css + 'tr[data-field]::attr(data-field)').extract()
         values = response.css(base_css + 'td.h .value span::text, ' + base_css + 'td.h:empty::attr(class)').extract()
+        diffs = response.css(base_css + 'td.h').extract()
+        diffs_processed = []
+
+        #TODO verify if regex are working
+        #TODO consider parametrizing
+        regex_empty = re.compile(r"^((?!class=\"changeqq\").)*$")
+        regex_yy = re.compile(r"<div class=\"changeyy\">r\/r <span class=\"pv\"><span><span class=\"q_ch_per (cplus|cminus)\">((\+|\-)\d+\.\d+%)<\/span><\/span><\/span>")
+        regex_sy = re.compile(r"<div class=\"changeyy\">r\/r <span class=\"pv\"><span><span class=\"q_ch_per (cplus|cminus)\">((\+|\-)\d+\.\d+%)<\/span><\/span><\/span><span class=\"sectorv\">~sektor <span class=\"pv\"><span><span class=\"q_ch_per (cplus|cminus)\">((\+|\-)\d+\.\d+%)<\/span><\/span><\/span><\/span></div>")
+        regex_qq = re.compile(r"<div class=\"changeqq\">k\/k <span class=\"pv\"><span><span class=\"q_ch_per (cplus|cminus)\">((\+|\-)\d+\.\d+%)<\/span><\/span><\/span>")
+        regex_sq = re.compile(r"<div class=\"changeqq\">k\/k <span class=\"pv\"><span><span class=\"q_ch_per (cplus|cminus)\">((\+|\-)\d+\.\d+%)<\/span><\/span><\/span><span class=\"sectorv\">~sektor <span class=\"pv\"><span><span class=\"q_ch_per (cplus|cminus)\">((\+|\-)\d+\.\d+%)<\/span><\/span><\/span><\/span></div>")
+        for diff in diffs:
+            if diff == '<td class="h"></td>':
+                diffs_processed += ['', '', '', '']
+            elif regex_empty.match(diff):
+                diffs_processed += ['', '', '', '']
+            else:
+                vals = []
+                match = regex_yy.search(diff)
+                if match:
+                    vals.append(match.group(2))
+                else:
+                    vals.append('')
+                match = regex_sy.search(diff)
+                if match:
+                    vals.append(match.group(5))
+                else:
+                    vals.append('')
+                match = regex_qq.search(diff)
+                if match:
+                    vals.append(match.group(2))
+                else:
+                    vals.append('')
+                match = regex_sq.search(diff)
+                if match:
+                    vals.append(match.group(5))
+                else:
+                    vals.append('')
+
+        labels_with_diffs = []
+        for label in labels:
+            labels_with_diffs += [label, label + '_rr', label + '_sr', label + '_qq', label + '_sq']
+        labels = labels_with_diffs
+
+        values_with_diffs = []
+        for value in values:
+            values_with_diffs.append(value)
+            for i in range(0,4):
+                values_with_diffs.append(diffs_processed.pop(0))
+        values = values_with_diffs
 
         quarters = list(map(lambda x: x.replace('\n', '').replace('\t', '').replace('\r', ''), quarters))
-        values = list(map(lambda x: x.replace('h', '').replace(' ', '').replace('newest', ''), values))
-
+        values = list(map(lambda x: x.replace('h', '').replace(' ', '').replace('newest', '').replace('+', '').replace('%', ''), values))
         # print(symbol)
         # print(quarters)
         # print(labels)
